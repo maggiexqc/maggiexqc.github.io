6ef16736ce2c10b6cd892685274d6e876ecdb82f
Maggie <103873589+maggiexqc@users.noreply.github.com>
2024-07-30 17:08:06 -0400

    added pages

--- js/opentransition.js
+++ js/opentransition.js
@@ -8,6 +8,6 @@ document.querySelectorAll('.app-card img').forEach(item => {
         // Wait for animation to complete before redirecting
         setTimeout(() => {
             window.location.href = this.parentElement.href;  // Redirect to the link in the parent <a> tag
-        }, 300); // Match this timeout with the length of your CSS animation
+        }, 100); // Match this timeout with the length of your CSS animation
     });
 });

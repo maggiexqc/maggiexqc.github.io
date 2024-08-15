9d4d78e5ba327a57fcbf5daf2d7b137d6f4c64a1
Maggie <103873589+maggiexqc@users.noreply.github.com>
2024-08-14 22:34:04 -0400

    Limited Scrolling

--- js/closetransition.js
+++ js/closetransition.js
@@ -1,11 +1,21 @@
-lastScrollTop = 0;
+let lastScrollTop = 0;
+let reachedBottom = false;
+
 window.addEventListener('scroll', function() {
     let currentScroll = window.scrollY;
-    // Check if user scrolled down
-    if (currentScroll > lastScrollTop) {
-        setTimeout(() => {
-            window.location.href = 'home.html'; 
-        }, 200); 
+    let maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+
+    // Check if the user has reached the bottom of the page
+    if (currentScroll >= maxScroll) {
+        reachedBottom = true; // Set the flag once the bottom is reached
     }
+
+    // If the user has already reached the bottom and scrolls down further
+    if (reachedBottom && currentScroll > lastScrollTop) {
+        setTimeout(() => {
+            window.location.href = 'home.html';
+        }, 200);
+    }
+
     lastScrollTop = currentScroll;
-});
+}, { passive: true });
